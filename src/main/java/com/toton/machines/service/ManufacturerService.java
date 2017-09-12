17bbe6d66e2f7653f22770febd0ab97c5095dccd
John Toton <john.toton1@ge.com>
2017-09-12 14:03:55 -0700

    add schema support;

--- src/main/java/com/toton/machines/service/ManufacturerService.java
+++ src/main/java/com/toton/machines/service/ManufacturerService.java
@@ -30,8 +30,8 @@ public class ManufacturerService {
 				"        ,m.id as manufacturer_id " + 
 				"        ,am.name as automobile_model_name " + 
 				"        ,am.id as automobile_model_id " + 
-				"        from manufacturer m " + 
-				"        left join automobile_model am on am.manufacturer_id = m.id " + 
+				"        from machines.manufacturer m " + 
+				"        left join machines.automobile_model am on am.manufacturer_id = m.id " + 
 				"        order by m.name, am.name " + 
 				"        ) models " + 
 				"    group by manufacturer_name, manufacturer_id " + 
